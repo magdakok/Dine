60acfa659bdfb8816cf3040281a45b9d7c38f632
magdakok <61601293+magdakok@users.noreply.github.com>
2020-05-10 19:12:06 +0100

    add test validation for name and email

--- script/form-validation.js
+++ script/form-validation.js
@@ -7,8 +7,36 @@ const year = document.getElementById('year');
 const hour = document.getElementById('hour');
 const minute = document.getElementById('minute');
 
+function correctName() {
+    if (!name.value) {
+        printError(name,'This field is required');
+    }
+};
 
+function correctEmail() {
+    if (!email.value) {
+        printError(email,'This field is required');
+    } 
+    //else if email doesn't contain @ printError(email, 'This is not an email address');
+};
+
+
+function printError (formElement, err) {
+    //get the parent element of the input
+    const parent = formElement.parentElement;
+    //add error class
+    parent.classList.add('error');
+    //get error message box
+    const small = parent.querySelector('small');
+    //prints error message
+    small.innerText = err;
+};
+
+// Event listeners
 form.addEventListener('submit', function(e){
     e.preventDefault();
-    console.log(name.value);
+    correctName();
+    correctEmail();
+    //here start all checking functions
 });
+
