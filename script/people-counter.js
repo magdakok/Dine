81f68c3d5ae929361ff346370e675b81695dc08e
magdakok <61601293+magdakok@users.noreply.github.com>
2020-05-13 16:21:13 +0100

    add mouseover and mouseout to plus and minus buttons

--- script/people-counter.js
+++ script/people-counter.js
@@ -2,6 +2,7 @@ const minus = document.getElementById('minus');
 const plus = document.getElementById('plus');
 const peopleNumber = document.getElementById('peopleNumber');
 const word = document.getElementById('personWord');
+const formPeople = document.querySelector('.form__people');
 let peopleCounter = 4;
 
 minus.addEventListener('click', ()=> {
@@ -26,4 +27,19 @@ plus.addEventListener('click', ()=> {
         peopleCounter++;
         peopleNumber.innerText = peopleCounter;
     }
+});
+
+
+// MOUSE OVER AND OUT PEOPLE NUMBER ELEMENT
+minus.addEventListener('mouseover', ()=> {
+    formPeople.classList.add('-plusminusActive');
+});
+minus.addEventListener('mouseout', ()=> {
+    formPeople.classList.remove('-plusminusActive');
+});
+plus.addEventListener('mouseover', ()=> {
+    formPeople.classList.add('-plusminusActive');
+});
+plus.addEventListener('mouseout', ()=> {
+    formPeople.classList.remove('-plusminusActive');
 });
