84645b26a9b662276eb6398de09b3cf3bf7232f8
magdakok <61601293+magdakok@users.noreply.github.com>
2020-05-16 18:35:35 +0100

    add shiny effect on buttons

---
+++ script/animation-button.js
@@ -0,0 +1,6 @@
+const buttons = document.querySelectorAll('.a-btnCta');
+const body = document.querySelector('body');
+
+setInterval(function(){
+    body.classList.add('-shineAnimation');
+}, 500);
