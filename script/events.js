e36778e737239c8256f4172cdb6a752482d83f86
magdakok <61601293+magdakok@users.noreply.github.com>
2020-05-16 17:37:28 +0100

    remove script preloading images and make them load automaticly when open website

--- script/events.js
+++ script/events.js
@@ -2,42 +2,25 @@ const events = [
     {
         title: 'Family Gathering',
         description: 'We love catering for entire families. So please bring everyone along for a special meal with your loved ones. We’ll provide a memorable experience for all.',
-        image: './images/homepage/family-gathering-mobile.jpg'
+        image: document.querySelector('#familyGatheringImg'),
     },
     {
         title: 'Special Events',
         description: 'Whether it’s a romantic dinner or special date you’re celebrating with others we’ll look after you. We’ll be sure to mark your special date with an unforgettable meal.',
-        image: './images/homepage/special-events-mobile.jpg'
+        image: document.querySelector('#specialEventsImg'),
     },
     {
         title: 'Social Events',
         description: 'Are you looking to have a larger social event? No problem! We’re more than happy to cater for big parties. We’ll work with you to make your event a hit with everyone.',
-        image: './images/homepage/social-events-mobile.jpg'
+        image: document.querySelector('#socialEventsImg'),
     }
 ];
 
-
-//preloads events images to display them without any wait
-let images = [];
-function preload() {
-    for (var i = 0; i < imags.length; i++) {
-        images[i] = new Image();
-        images[i].src = preload.imgs[i];
-    }
-}
-
-preload(
-    "./images/homepage/family-gathering-mobile.jpg",
-    "./images/homepage/special-events-mobile.jpg",
-    "./images/homepage/social-events-mobile.jpg"
-)
-
-
 // DOM VARIABLES
-const eventImg = document.querySelector('#eventImg');
 const eventHeading = document.querySelector('#eventHeading');
 const eventText = document.querySelector('#eventText');
 const menuItems = document.querySelectorAll('.m-events__item');
+const imgBoxItems = document.querySelectorAll('.m-event__img');
 
 
 // CLICK ON MENU ITEM
@@ -45,9 +28,10 @@ menuItems.forEach((item,i) => {
     item.addEventListener('click', function(){
         eventHeading.innerText = events[i].title;
         eventText.innerText = events[i].description;
-        eventImg.src = events[i].image;
-
-        //works but it's ugly, must be better way to remove active class:
+        imgBoxItems.forEach(item => {
+            item.classList.remove('-visible');
+         })
+        events[i].image.classList.add('-visible');
         menuItems.forEach(item => {
             item.classList.remove('-active');
         })
